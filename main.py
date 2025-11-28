6fca93efa7cb784c42e3c0f870f4607fc8090483
amirsolobass <amirsolobass@gmail.com>
2025-11-28 12:29:08 +0100

    Moved most functions to stats.py and made it pretty

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+from stats import count_words, count_chars, sort_list
+
 def get_book_text(file_path):
-    with open(file_path) as file:
-        book_text = file.read()
+    with open(file_path) as file: # Open the book file
+        book_text = file.read() # Read the entire content
         return book_text
+
 
 def main():
     book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
-    print(book_text)
-
+    book_text = get_book_text(book_path) # Get book text
+    count = count_words(book_text) # Count words
+    char_counts = count_chars(book_text) # Count characters
+    char_counts = sort_list(char_counts) # Sort character counts
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book at: {book_path}")
+    print("=================================")
+    print(f"Found {count} total words")
+    print("--------- Character Count -------")
+    for char, count in char_counts: # Print each character and its count as a list
+        print(f"{char}: {count}")
+    print("============= END ===============")
+    
 main()
